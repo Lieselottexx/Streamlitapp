9b5d2db31990dc9867afe0d0bf5ef2078846e7dd
Lieselottexx <85686766+Lieselottexx@users.noreply.github.com>
2025-04-08 13:53:18 +0200

    rückgabe erweitert, immer noch bad message sessioninfo error

--- NewControl.py
+++ NewControl.py
@@ -109,7 +109,7 @@ class Control():
         status_text_Opti1.text(f"Optimierter Lastgang wird berechnet... {progress_Opti1}% abgeschlossen")
         
         
-        data_optimised = self.opimisation.select_optimisation(self.data.astype(Param.datatype), 
+        data_optimised, session = self.opimisation.select_optimisation(self.data.astype(Param.datatype), 
                                                               input_optimisation, 
                                                               select_opti, session)
         print("1. Opti fertig")
@@ -133,9 +133,9 @@ class Control():
         progress_Opti2 = 10
         progress_bar_Opti2.progress(progress_Opti2)
         status_text_Opti2.text(f"Eigenverbrauchsoptimierung wird berechnet... {progress_Opti2}% abgeschlossen")
-        # data_optimised = self.opimisation.select_optimisation(self.data.astype(Param.datatype), 
-        #                                                       input_optimisation, 
-        #                                                       select_opti, session)
+        data_optimised, session = self.opimisation.select_optimisation(self.data.astype(Param.datatype), 
+                                                              input_optimisation, 
+                                                              select_opti, session)
         print("zweite Opti fertig")
         progress_Opti2 = 90
         progress_bar_Opti2.progress(progress_Opti2)
