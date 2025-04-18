8177a4bbbf51abbac4e69fb8f37f3b43c97d1cca
Laura Weghake <85686766+Lieselottexx@users.noreply.github.com>
2025-04-18 11:22:03 +0200

    10min

--- NewDataGenerator.py
+++ NewDataGenerator.py
@@ -52,7 +52,7 @@ class DataGenerator():
         				
         # Resample the Data to 15 min 
         # Preise behalten den ersten und Energiedaten werden aufsummiert
-        data_resample = data.resample('15T').agg({
+        data_resample = data.resample('10T').agg({
                                                     "Load Energy [kWh]"                 : "sum",
                                                     "SLP-Energy [kWh]"                  : "sum",
                                                     "PV-Energy [kWh]"                   : "sum",
