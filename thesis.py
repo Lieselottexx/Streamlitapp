61bdc147fd49bbcdc292be72001852c0b44db521
Laura Weghake <85686766+Lieselottexx@users.noreply.github.com>
2025-10-26 17:56:51 +0100

    Comment out author and contact markdown
    Comment out author and contact information.

--- thesis.py
+++ thesis.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 st.title("Erweiterte Ergebnisse")
-st.markdown(""":blue[Entwickelt von Laura Weghake B. Eng.] """)
-st.markdown(""":blue[Fragen und Anregungen gerne an l.weghake@gmail.com]""")
+# st.markdown(""":blue[Entwickelt von Laura Weghake B. Eng.] """)
+# st.markdown(""":blue[Fragen und Anregungen gerne an l.weghake@gmail.com]""")
 
 
 st.warning("Auf dieser Seite ist aktuell noch Baustelle!")
