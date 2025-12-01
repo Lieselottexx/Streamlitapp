f3a4d1798c2270494ffdde98510b0293a4ccb38b
Laura Weghake <85686766+Lieselottexx@users.noreply.github.com>
2025-12-01 12:52:18 +0100

    Uncomment author and contact information

--- explain.py
+++ explain.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 
 st.title("🔌 Einschätzung zum Wechsel auf einen dynamischen Stromtarif")
-# st.markdown(""":blue[Entwickelt von Laura Weghake B. Eng.] """)
-# st.markdown(""":blue[Fragen und Anregungen gerne an l.weghake@gmail.com]""")
+st.markdown(""":blue[Entwickelt von Laura Weghake B. Eng.] """)
+st.markdown(""":blue[Fragen und Anregungen gerne an l.weghake@gmail.com]""")
 st.markdown("Auf dieser Seite werden alle Einstellmöglichkeiten sowie die Annahmen des Rechners erklärt. Des Weiteren sind auf dieser Seite der Optimierungsprozess und die Interpretation des Ergebnisses erklärt.")
 
 
